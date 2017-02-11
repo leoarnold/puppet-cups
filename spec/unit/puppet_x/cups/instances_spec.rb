deac486363e83579fe6931d678bddc25edf14d62
Leo Arnold <github@leoarnold.de>
2017-02-11 08:19:05 +0100

    Make spec testing more literate

--- spec/unit/puppet_x/cups/instances_spec.rb
+++ spec/unit/puppet_x/cups/instances_spec.rb
@@ -1,67 +1,85 @@
 require 'spec_helper'
 require 'lib/puppet_x/cups/instances'
 
-def mock_queues_rows(printers, classmembers)
-  response_mock = instance_double(PuppetX::Cups::Ipp::Response)
-  allow(response_mock).to receive(:rows).and_return(printers + classmembers.keys)
-  allow(PuppetX::Cups::Ipp).to receive(:query).with(PuppetX::Cups::Instances::Queues.request).and_return(response_mock)
+def cups_get_printers
+  response = PuppetX::Cups::Ipp::Response.new(yield)
+  allow(PuppetX::Cups::Ipp).to receive(:query)
+    .with(PuppetX::Cups::Instances::Queues.request)
+    .and_return(response)
 end
 
-def mock_classmembers_rows(classmembers)
-  response_mock = instance_double(PuppetX::Cups::Ipp::Response)
-  allow(response_mock).to receive(:rows).and_return(classmembers_rows(classmembers))
-  allow(PuppetX::Cups::Ipp).to receive(:query).with(PuppetX::Cups::Instances::ClassMembers.request).and_return(response_mock)
-end
-
-def classmembers_rows(classmembers)
-  response = []
-  classmembers.keys.each do |queue|
-    members = classmembers[queue].join(',')
-    members = '"' + members + '"' if members.include? ','
-    response.push(queue + ',' + members)
-  end
-  response
+def cups_get_classes
+  response = PuppetX::Cups::Ipp::Response.new(yield)
+  allow(PuppetX::Cups::Ipp).to receive(:query)
+    .with(PuppetX::Cups::Instances::ClassMembers.request)
+    .and_return(response)
 end
 
 describe PuppetX::Cups::Instances::Classes do
   describe '#to_a' do
     context 'without printers or classes installed' do
       it 'returns an empty array' do
-        classmembers = {}
-        printers = []
-        expected = []
+        cups_get_classes do <<-EOT
+printer-name,member-names
+          EOT
+        end
 
-        mock_classmembers_rows(classmembers)
-        mock_queues_rows(printers, classmembers)
-        expect(described_class.to_a).to match_array(expected)
+        cups_get_printers do <<-EOT
+printer-name
+          EOT
+        end
+
+        expectation = []
+
+        expect(described_class.to_a).to match_array(expectation)
       end
     end
 
     context 'with printers, but without classes installed' do
       it 'returns an array with the names of all installed printers' do
-        classmembers = {}
-        printers = %w(BackOffice Office Warehouse)
-        expected = []
+        cups_get_classes do <<-EOT
+printer-name,member-names
+          EOT
+        end
 
-        mock_classmembers_rows(classmembers)
-        mock_queues_rows(printers, classmembers)
-        expect(described_class.to_a).to match_array(expected)
+        cups_get_printers do <<-EOT
+printer-name
+BackOffice
+Office
+Warehouse
+          EOT
+        end
+
+        expectation = []
+
+        expect(described_class.to_a).to match_array(expectation)
       end
     end
 
     context 'with printers and classes installed' do
       it 'returns an array with the names of all installed printers, including classes' do
-        classmembers = {
-          'CrawlSpace'  => %w(),
-          'GroundFloor' => %w(Office Warehouse),
-          'UpperFloor'  => %w(BackOffice)
-        }
-        printers = %w(BackOffice Office Warehouse)
-        expected = %w(CrawlSpace GroundFloor UpperFloor)
+        cups_get_classes do <<-EOT
+printer-name,member-names
+CrawlSpace,
+GroundFloor,"Office,Warehouse"
+UpperFloor,BackOffice
+          EOT
+        end
 
-        mock_classmembers_rows(classmembers)
-        mock_queues_rows(printers, classmembers)
-        expect(described_class.to_a).to match_array(expected)
+        cups_get_printers do <<-EOT
+printer-name
+BackOffice
+CrawlSpace
+GroundFloor
+Office
+UpperFloor
+Warehouse
+          EOT
+        end
+
+        expectation = %w(CrawlSpace GroundFloor UpperFloor)
+
+        expect(described_class.to_a).to match_array(expectation)
       end
     end
   end
@@ -71,33 +89,53 @@ describe PuppetX::Cups::Instances::ClassMembers do
   describe '#to_h' do
     context 'with no classes installed' do
       it 'returns an empty hash' do
-        classmembers = {}
-        printers = %w(BackOffice Office Warehouse)
-        expected = {}
+        cups_get_classes do <<-EOT
+printer-name,member-names
+          EOT
+        end
 
-        mock_classmembers_rows(classmembers)
-        mock_queues_rows(printers, classmembers)
-        expect(described_class.to_h).to match_array(expected)
+        cups_get_printers do <<-EOT
+printer-name
+BackOffice
+Office
+Warehouse
+          EOT
+        end
+
+        expectation = {}
+
+        expect(described_class.to_h).to match_array(expectation)
       end
     end
 
     context 'with classes installed' do
       it 'returns the correct hash' do
-        classmembers = {
-          'CrawlSpace'  => %w(),
-          'GroundFloor' => %w(Office Warehouse),
-          'UpperFloor'  => %w(BackOffice)
-        }
-        printers = %w(BackOffice Office Warehouse)
-        expected = {
+        cups_get_classes do <<-EOT
+printer-name,member-names
+CrawlSpace,
+GroundFloor,"Office,Warehouse"
+UpperFloor,BackOffice
+          EOT
+        end
+
+        cups_get_printers do <<-EOT
+printer-name
+BackOffice
+CrawlSpace
+GroundFloor
+Office
+UpperFloor
+Warehouse
+          EOT
+        end
+
+        expectation = {
           'CrawlSpace'  => %w(),
           'GroundFloor' => %w(Office Warehouse),
           'UpperFloor'  => %w(BackOffice)
         }
 
-        mock_classmembers_rows(classmembers)
-        mock_queues_rows(printers, classmembers)
-        expect(described_class.to_h).to match_array(expected)
+        expect(described_class.to_h).to match_array(expectation)
       end
     end
   end
@@ -107,41 +145,67 @@ describe PuppetX::Cups::Instances::Printers do
   describe '#to_a' do
     context 'without printers or classes installed' do
       it 'returns an empty array' do
-        classmembers = {}
-        printers = []
-        expected = []
+        cups_get_classes do <<-EOT
+printer-name,member-names
+          EOT
+        end
 
-        mock_classmembers_rows(classmembers)
-        mock_queues_rows(printers, classmembers)
-        expect(described_class.to_a).to match_array(expected)
+        cups_get_printers do <<-EOT
+printer-name
+          EOT
+        end
+
+        expectation = []
+
+        expect(described_class.to_a).to match_array(expectation)
       end
     end
 
     context 'with printers, but without classes installed' do
       it 'returns an array with the names of all installed printers' do
-        classmembers = {}
-        printers = %w(BackOffice Office Warehouse)
-        expected = %w(BackOffice Office Warehouse)
+        cups_get_classes do <<-EOT
+printer-name,member-names
+          EOT
+        end
 
-        mock_classmembers_rows(classmembers)
-        mock_queues_rows(printers, classmembers)
-        expect(described_class.to_a).to match_array(expected)
+        cups_get_printers do <<-EOT
+printer-name
+BackOffice
+Office
+Warehouse
+          EOT
+        end
+
+        expectation = %w(BackOffice Office Warehouse)
+
+        expect(described_class.to_a).to match_array(expectation)
       end
     end
 
     context 'with printers and classes installed' do
       it 'returns an array with the names of all installed printers, including classes' do
-        classmembers = {
-          'CrawlSpace'  => %w(),
-          'GroundFloor' => %w(Office Warehouse),
-          'UpperFloor'  => %w(BackOffice)
-        }
-        printers = %w(BackOffice Office Warehouse)
-        expected = %w(BackOffice Office Warehouse)
+        cups_get_classes do <<-EOT
+printer-name,member-names
+CrawlSpace,
+GroundFloor,"Office,Warehouse"
+UpperFloor,BackOffice
+          EOT
+        end
 
-        mock_classmembers_rows(classmembers)
-        mock_queues_rows(printers, classmembers)
-        expect(described_class.to_a).to match_array(expected)
+        cups_get_printers do <<-EOT
+printer-name
+BackOffice
+CrawlSpace
+GroundFloor
+Office
+UpperFloor
+Warehouse
+          EOT
+        end
+
+        expectation = %w(BackOffice Office Warehouse)
+
+        expect(described_class.to_a).to match_array(expectation)
       end
     end
   end
@@ -151,29 +215,46 @@ describe PuppetX::Cups::Instances::Queues do
   describe '#to_a' do
     context 'without queues installed' do
       it 'returns an empty array' do
-        classmembers = {}
-        printers = []
-        expected = []
+        cups_get_classes do <<-EOT
+printer-name,member-names
+          EOT
+        end
 
-        mock_classmembers_rows(classmembers)
-        mock_queues_rows(printers, classmembers)
-        expect(described_class.to_a).to match_array(expected)
+        cups_get_printers do <<-EOT
+printer-name
+          EOT
+        end
+
+        expectation = []
+
+        expect(described_class.to_a).to match_array(expectation)
       end
     end
 
     context 'with queues installed' do
       it 'returns an array with the names of all installed queues' do
-        classmembers = {
-          'CrawlSpace'  => %w(),
-          'GroundFloor' => %w(Office Warehouse),
-          'UpperFloor'  => %w(BackOffice)
-        }
-        printers = %w(BackOffice Office Warehouse)
-        expected = %w(CrawlSpace BackOffice GroundFloor Office UpperFloor Warehouse)
+        cups_get_classes do <<-EOT
+printer-name,member-names
+CrawlSpace,
+GroundFloor,"Office,Warehouse"
+UpperFloor,BackOffice
+          EOT
+        end
 
-        mock_classmembers_rows(classmembers)
-        mock_queues_rows(printers, classmembers)
-        expect(described_class.to_a).to match_array(expected)
+        cups_get_printers do <<-EOT
+printer-name
+BackOffice
+CrawlSpace
+GroundFloor
+Office
+UpperFloor
+Warehouse
+          EOT
+        end
+
+        expectation = %w(CrawlSpace BackOffice GroundFloor Office UpperFloor Warehouse)
+
+        expect(described_class.to_a).to match_array(expectation)
       end
     end
   end
