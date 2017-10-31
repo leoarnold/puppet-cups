0d2fd3bfc1ee635b70d7b418a95ab751f04de46e
Leo Arnold <github@leoarnold.de>
2017-10-31 18:57:16 +0100

    Correct coding style

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -40,7 +40,7 @@ RSpec.configure do |c|
   c.color = true
   c.formatter = :documentation
   c.mock_with(:rspec)
-  c.example_status_persistence_file_path = ".rspec_status"
+  c.example_status_persistence_file_path = '.rspec_status'
 end
 
 # RSpec-Puppet configuration
